6fcf7e38add936818cb1a71d19bc6339c7d89ac7
yy0608 <447462224@qq.com>
2018-03-24 17:43:52 +0800

    图片上传组件，采用上传之后再转移位置

--- routers/clothes/qiniu.js
+++ routers/clothes/qiniu.js
@@ -1,8 +1,5 @@
 var express = require('express');
 var qiniu = require('qiniu');
-// var multer = require('multer');
-// var bytes = require('bytes');
-var multiparty = require('multiparty');
 
 var config = require('./config.js');
 
@@ -11,16 +8,16 @@ var router = express.Router();
 var qiniuObj = {}; // 当前页的全局对象
 
 router.post('/generate_token', function (req, res, next) {
-  qiniuObj.accessKey = qiniuObj.accessKey || config.qiniuConfig.access_key;
-  qiniuObj.secretKey = qiniuObj.secretKey || config.qiniuConfig.secret_key;
-  qiniuObj.mac = qiniuObj.mac || new qiniu.auth.digest.Mac(qiniuObj.accessKey, qiniuObj.secretKey);
-
-  var putPolicy = new qiniu.rs.PutPolicy({ scope: 'wusuowei' });
-  var uploadToken = putPolicy.uploadToken(qiniuObj.mac);
+  var reqBody = req.body;
+  var scope = reqBody.scope || config.qiniuConfig.default_bucket;
+  var expires = parseInt(reqBody.expires);
+  expires = isNaN(expires) ? 3600 : expires; // 单位秒
+  var token = generateToken(scope, expires);
 
   res.json({
     success: true,
-    data: uploadToken
+    data: token,
+    expires: expires
   })
 })
 
@@ -41,21 +38,13 @@ router.post('/generate_token', function (req, res, next) {
 //   console.log(req.file)
 // })
 
-router.post('/resource_upload', function (req, res, next) {
-  var form = new multiparty.Form();
-  form.parse(req, function (err, fields, files) {
-    if (err) {
-      return console.log(err)
-    }
-    console.log(111, fields)
-    console.log(222, files)
-  })
+router.post('/resource_upload', function (req, res, next) { // 资源上传
   res.json({
     success: true
   })
 })
 
-router.post('/resource_stat', function (req, res, next) {
+router.post('/resource_stat', function (req, res, next) { // 资源信息
   var reqBody = req.body;
   var bucket = reqBody.bucket || config.qiniuConfig.default_bucket;
   var resourceKey = reqBody.key
@@ -68,32 +57,127 @@ router.post('/resource_stat', function (req, res, next) {
 
   qiniuObj.bucketManager = qiniuObj.bucketManager || generateBucketManager();
 
-  qiniuObj.bucketManager.stat(bucket, resourceKey, function (err, data) {
+  qiniuObj.bucketManager.stat(bucket, resourceKey, function (err, respBody, respInfo) {
     if (err) {
-      res.json({
+      return res.json({
         sucess: false,
         msg: '获取资源信息出错',
         err: err.toString()
       })
+    }
+    if (respInfo.statusCode === 200) {
+      res.json({
+        success: true,
+        msg: '获取资源信息成功',
+        data: respInfo.data
+      })
     } else {
-      if (data.error) {
-        res.json({
-          sucess: false,
-          msg: '获取资源信息出错',
-          err: data.error
-        })
-      } else {
-        res.json({
-          success: true,
-          msg: '获取资源信息成功',
-          data: data
-        })
-      }
+      res.json({
+        sucess: false,
+        msg: '获取资源信息出错',
+        err: respInfo.data.error
+      })
     }
   })
 })
 
-router.post('/resource_delete', function (req, res, next) {
+router.post('/resource_list', function (req, res, next) { // 获取资源列表
+  var reqBody = req.body;
+  var bucket = reqBody.bucket || config.qiniuConfig.default_bucket;
+  var prefix = reqBody.prefix || '' // 前缀
+  var limit = reqBody.limit || config.pageLimit // 每次几条
+  var marker = reqBody.marker || '' // 上一次的标记
+  var options = {
+    prefix: prefix,
+    limit: limit,
+    marker: marker
+  }
+
+  qiniuObj.bucketManager = qiniuObj.bucketManager || generateBucketManager();
+
+  qiniuObj.bucketManager.listPrefix(bucket, options, function (err, respBody, respInfo) {
+    if (err) {
+      res.json({
+        success: false,
+        msg: '获取七牛资源列表失败',
+        err: err
+      })
+    }
+    if (respInfo.statusCode == 200) {
+      //如果这个nextMarker不为空，那么还有未列举完毕的文件列表，下次调用listPrefix的时候，
+      //指定options里面的marker为这个值
+      var nextMarker = respBody.marker;
+      var commonPrefixes = respBody.commonPrefixes;
+      var resList = []
+      // console.log(nextMarker);
+      // console.log(commonPrefixes);
+      var items = respBody.items;
+      items.forEach(function(item) {
+        resList.push({
+          key: item.key,
+          type: item.mimetype,
+          fsize: item.fsize,
+          putTime: item.putTime
+        })
+      });
+      res.json({
+        success: true,
+        msg: '获取七牛资源列表成功',
+        nextMarker: nextMarker,
+        data: resList
+      })
+    } else {
+      res.json({
+        success: false,
+        msg: '获取七牛资源列表失败',
+        err: respInfo.data.error
+      })
+      // console.log(respInfo.statusCode);
+      // console.log(respBody);
+    }
+  });
+})
+
+router.post('/resource_move', function (req, res, next) { // 资源移动或重命名
+  var reqBody = req.body;
+  var srcKey = reqBody.srcKey;
+  var destKey = reqBody.destKey;
+
+  if (!srcKey || !destKey) {
+    return res.json({
+      success: false,
+      msg: '缺少参数'
+    })
+  }
+
+  var srcBucket = reqBody.srcBucket || config.qiniuConfig.default_bucket;
+  var destBucket = reqBody.destBucket || config.qiniuConfig.default_bucket;
+
+  qiniuObj.bucketManager = qiniuObj.bucketManager || generateBucketManager();
+
+  qiniuObj.bucketManager.move(srcBucket, srcKey, destBucket, destKey, { force: !!reqBody.force }, function (err, respBody, respInfo) {
+    if (err) {
+      return res.json({
+        success: false,
+        msg: '资源移动失败'
+      })
+    }
+    if (respInfo.statusCode === 200) {
+      res.json({
+        success: true,
+        msg: '资源移动成功'
+      })
+    } else {
+      res.json({
+        success: false,
+        msg: '资源移动失败',
+        err: respInfo.data.error
+      })
+    }
+  })
+})
+
+router.post('/resource_delete', function (req, res, next) { // 资源删除
   var reqBody = req.body;
   var bucket = reqBody.bucket || config.qiniuConfig.default_bucket;
   var resourceKey = reqBody.key
@@ -132,7 +216,18 @@ router.post('/resource_delete', function (req, res, next) {
   })
 })
 
-function generateBucketManager () {
+function generateToken (scope, expires) { // 生成token
+  qiniuObj.accessKey = qiniuObj.accessKey || config.qiniuConfig.access_key;
+  qiniuObj.secretKey = qiniuObj.secretKey || config.qiniuConfig.secret_key;
+  qiniuObj.mac = qiniuObj.mac || new qiniu.auth.digest.Mac(qiniuObj.accessKey, qiniuObj.secretKey);
+
+  var putPolicy = new qiniu.rs.PutPolicy({ scope: scope, expires: expires });
+  var token = putPolicy.uploadToken(qiniuObj.mac);
+
+  return token
+}
+
+function generateBucketManager () { // 生成管理
   qiniuObj.accessKey = qiniuObj.accessKey || config.qiniuConfig.access_key;
   qiniuObj.secretKey = qiniuObj.secretKey || config.qiniuConfig.secret_key;
 
